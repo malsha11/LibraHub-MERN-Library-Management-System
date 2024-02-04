7ee4f067180fe469400ec8dcdd6c3da88c976d83
MalshaDissanayaka11 <admsewwandi@gmail.com>
2024-02-04 21:13:56 +0530

    Add Middlewares

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+
+// Middlewares
+app.use("/",(req, res, next) => {
+    res.send("This is our starting App");
+});
+
+
 mongoose.connect("mongodb+srv://admin:w6OGY9MzQFo6SnEB@cluster0.tb42d32.mongodb.net/LibraHub Library Management System?retryWrites=true&w=majority")
 .then(() => console.log("Connected To Database"))
   .then(() => {
