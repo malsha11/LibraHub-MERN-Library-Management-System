802680631b7cc4a97dc1a41980bc9eb244a8d1d6
MalshaDissanayaka11 <admsewwandi@gmail.com>
2024-02-04 23:38:21 +0530

    Add books funtion

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,22 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Book = require("../model/Book");
+const booksController = require("../controllers/books-controller");
 
 
-// Middlewares
-router.get("/", async (req, res, next) => {
-    // This routes will be provide all of the books
-    let books;
-    try{
-        books = await Book.find();
-    }catch(err){
-        console.log(err);
-    }
-    if(!books) {
-        return res.status(404).json({message:"No books found"});
-    }
-        return res.status(200).json({books});
-
-});
+router.get("/", booksController.getAllBooks);
+router.post("/", booksController.addBook);
 
 module.exports = router
