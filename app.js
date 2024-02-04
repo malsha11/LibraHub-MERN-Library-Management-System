f0a72596567db632f9b4c6fe03da5441f779cba9
MalshaDissanayaka11 <admsewwandi@gmail.com>
2024-02-04 21:06:42 +0530

    Connected To Database

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello world!!");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+mongoose.connect("mongodb+srv://admin:w6OGY9MzQFo6SnEB@cluster0.tb42d32.mongodb.net/LibraHub Library Management System?retryWrites=true&w=majority")
+.then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
