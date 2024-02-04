66137ea02a0de3af5cf0f3c57664164408417d46
MalshaDissanayaka11 <admsewwandi@gmail.com>
2024-02-04 21:47:19 +0530

    changed Middlewares

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+
 
 const app = express();
 
 // Middlewares
-app.use("/",(req, res, next) => {
-    res.send("This is our starting App");
-});
+app.use("/books", router); // localhost:5000/books
 
 
 mongoose.connect("mongodb+srv://admin:w6OGY9MzQFo6SnEB@cluster0.tb42d32.mongodb.net/LibraHub Library Management System?retryWrites=true&w=majority")
