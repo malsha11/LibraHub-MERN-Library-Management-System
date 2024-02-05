bb8aac0b58c1a83dc8cf186f926ee4eec9c21c1f
MalshaDissanayaka11 <admsewwandi@gmail.com>
2024-02-05 11:08:50 +0530

    changed DB name

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use("/books", router); // localhost:5000/books
 
 
-mongoose.connect("mongodb+srv://admin:w6OGY9MzQFo6SnEB@cluster0.tb42d32.mongodb.net/LibraHub Library Management System?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://admin:w6OGY9MzQFo6SnEB@cluster0.tb42d32.mongodb.net/librahub_library_management_system?retryWrites=true&w=majority")
 .then(() => console.log("Connected To Database"))
   .then(() => {
     app.listen(5000);
